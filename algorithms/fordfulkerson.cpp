5c2907624fc96777b39dd522f423651025e42b8b
pasreis <pas_reis@hotmail.com>
2020-05-28 16:10:21 +0100

    Some beautification done

--- algorithms/fordfulkerson.cpp
+++ algorithms/fordfulkerson.cpp
@@ -1,7 +1,36 @@
+/******************************************************************************
+ *
+ * File Name: karger.cpp
+ * Author:    Gonçalo Mestre & Pedro Reis
+ * Last Modification: 28 May 2020
+ *
+ * NAME
+ *  fordfulkerson - functions used to implement Ford-Fulkerson's algorithm
+ *
+ * DESCRIPTION
+ *  This file contains the implementation of all functions used in the
+ * implementation of Ford-Fulkerson's algorithm for the computation of the 
+ * minimum cut size by using the Min-Cut Max-Flow Theorem. This done for three 
+ * different graph representations.
+ *
+ *****************************************************************************/
+
 #include "fordfulkerson.hpp"
 
 std::function<int (int, int)> min = [](int a, int b) { return (a < b) ? a : b; };
 
+/**
+ * hasMorePaths: Computes an incrementing path from ´source´ to ´sink´ using
+ * DFS algorithm.
+ * 
+ * \param residualGraph: graph where to look
+ * \param src: source node
+ * \param sink: destination node
+ * \param parent[]: array of nodes' parents
+ * 
+ * \return true if it founds any path and stores it in parent or false
+ * otherwise
+ */
 bool hasMorePaths(Graph* residualGraph, int src, int sink, int parent[]) {
 	bool visited[residualGraph->getV()];
 	memset(visited, 0, sizeof(visited));
@@ -28,9 +57,18 @@ bool hasMorePaths(Graph* residualGraph, int src, int sink, int parent[]) {
 	return (visited[sink] == true);
 }
 
+/**
+ * fordfulkerson: Computes Ford-Fulkerson Algorithm to obtain Maximum Flow
+ * between source and sink. Uses Adjacency Matrix data-structure for representng
+ * the graph
+ * 
+ * \param graph: represented by Adjacency Matrix data structure
+ * \param src: source of the flow
+ * \param sink: sink of the flow
+ * 
+ * \return: maximum flow between src and sink
+ */
 int fordfulkerson(AdjMatrix* graph, int src, int sink) {
-	//int src = 0; // first vertice
-	//int sink = graph->getV() - 1; // last vertice
 	int v, u;
 
 	AdjMatrix* residualGraph = new AdjMatrix(graph->getV());
@@ -62,6 +100,15 @@ int fordfulkerson(AdjMatrix* graph, int src, int sink) {
 	
 }
 
+/**
+ * fordfulkerson: omputes Ford-Fulkerson Algorithm to obtain the Minimum Cut
+ * by using the Min-Cut Max-Flow Theorem. Uses Adjacency Matrix data-structure 
+ * for representng the graph.
+ * 
+ * \param graph represented by Adjacency Matrix data structure
+ * 
+ * \return Minimum Cut size for graph
+ */
 int fordfulkerson(AdjMatrix* graph) {
 	int mincut = INT_MAX;
 	int maxFlow = 0;
@@ -79,9 +126,18 @@ int fordfulkerson(AdjMatrix* graph) {
 	return mincut;
 }
 
+/**
+ * fordfulkerson: Computes Ford-Fulkerson Algorithm to obtain Maximum Flow
+ * between source and sink. Uses Adjacency List data-structure for representng
+ * the graph
+ * 
+ * \param graph: represented by Adjacency List data structure
+ * \param src: source of the flow
+ * \param sink: sink of the flow
+ * 
+ * \return: maximum flow between src and sink
+ */
 int fordfulkerson(AdjList* graph, int src, int sink) {
-	//int src = 0; // first vertice
-	//int sink = graph->getV() - 1; // last vertice
 	int u, v;
 
 	AdjList* residualGraph = new AdjList(*graph);
@@ -110,6 +166,15 @@ int fordfulkerson(AdjList* graph, int src, int sink) {
 	return maxFlow;
 }
 
+/**
+ * fordfulkerson: omputes Ford-Fulkerson Algorithm to obtain the Minimum Cut
+ * by using the Min-Cut Max-Flow Theorem. Uses Adjacency List data-structure 
+ * for representng the graph.
+ * 
+ * \param graph represented by Adjacency List data structure
+ * 
+ * \return Minimum Cut size for graph
+ */
 int fordfulkerson(AdjList* graph) {
 	int mincut = INT_MAX;
 	int maxFlow = 0;
@@ -127,6 +192,17 @@ int fordfulkerson(AdjList* graph) {
 	return mincut;
 }
 
+/**
+ * fordfulkerson: Computes Ford-Fulkerson Algorithm to obtain Maximum Flow
+ * between source and sink. Uses CSR data-structure for representng the 
+ * graph
+ * 
+ * \param graph: represented by CSR data structure
+ * \param src: source of the flow
+ * \param sink: sink of the flow
+ * 
+ * \return: maximum flow between src and sink
+ */
 int fordfulkerson(CSRGraph* graph, int src, int sink) {
 	//int src = 0; // first vertice
 	//int sink = graph->getV() - 1; // last vertice
@@ -164,6 +240,15 @@ int fordfulkerson(CSRGraph* graph, int src, int sink) {
 	return maxFlow;
 }
 
+/**
+ * fordfulkerson: omputes Ford-Fulkerson Algorithm to obtain the Minimum Cut
+ * by using the Min-Cut Max-Flow Theorem. Uses CSR data-structure for 
+ * representng the graph.
+ * 
+ * \param graph represented by CSR data structure
+ * 
+ * \return Minimum Cut size for graph
+ */
 int fordfulkerson(CSRGraph* graph) {
 	int mincut = INT_MAX;
 	int maxFlow = 0;
--- algorithms/fordfulkerson.hpp
+++ algorithms/fordfulkerson.hpp
@@ -1,3 +1,20 @@
+/******************************************************************************
+ *
+ * File Name: karger.cpp
+ * Author:    Gonçalo Mestre & Pedro Reis
+ * Last Modification: 28 May 2020
+ *
+ * NAME
+ *  fordfulkerson - functions used to implement Ford-Fulkerson's algorithm
+ *
+ * DESCRIPTION
+ *  This file contains the implementation of all functions used in the
+ * implementation of Ford-Fulkerson's algorithm for the computation of the 
+ * minimum cut size by using the Min-Cut Max-Flow Theorem. This done for three 
+ * different graph representations.
+ *
+ *****************************************************************************/
+
 #ifndef FORDFULKERSON_H
 #define FORDFULKERSON_H
 
